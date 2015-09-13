2ecc581266cb11ea75915cdd2f35757c2bee504a
unknown <ravicool74@gmail.com>
2015-09-14 03:18:30 +0530

    Added User Location Modal Window

--- app/user/user.js
+++ app/user/user.js
@@ -13,16 +13,34 @@ angular.module('myApp.user', ['ngRoute'])
 function init(){
   $scope.viewOverview = true;
   $scope.viewAccountSettings = false;
+  $scope.searchAnything = false;
+  $scope.userDetails = $scope.currentUser = {'empId' : '01987', 'emailId' : 'bhagat@freedom.com','team': 'KatyFlyers', 'mobileNumber': '+91-9493645007', 'voipNumber': '+71-402-544-4848', 'deskLocation': 'B6-064'};
+};
+
+function resetFlags(){
+  $scope.viewOverview = false;
+  $scope.viewAccountSettings = false;
+  $scope.searchAnything = false;
 };
 
 $scope.viewOverviewFun = function(){
+  resetFlags();
   $scope.viewOverview = true;
-  $scope.viewAccountSettings = false;
+  $scope.userDetails = $scope.currentUser;
 };
 
 $scope.viewAccountSettingsFun = function(){
-  $scope.viewOverview = false;
+  resetFlags();
   $scope.viewAccountSettings = true;
+};
+
+$scope.search = function(){
+  resetFlags();
+  $scope.searchAnything = true;
+}
+
+$scope.selectedCabin = function(){
+  $('#'+$scope.userDetails.deskLocation.replace('-','')).addClass('selectedPlace');
 };
 init();
 }]);
