2071648c8c01769ed08cc9486d922517c631f210
unknown <ravicool74@gmail.com>
2015-09-21 09:47:47 +0530

    Updated User Profile Page and added Search Page

--- app/user/user.js
+++ app/user/user.js
@@ -14,6 +14,8 @@ function init(){
   $scope.viewOverview = true;
   $scope.viewAccountSettings = false;
   $scope.searchAnything = false;
+  $scope.searchOptions =['Name', 'Employee Id', 'Team'];
+  $scope.user = {'name' : 'Bhaskar Matam', 'designation' : 'Software Engineer'};
   $scope.userDetails = $scope.currentUser = {'empId' : '01987', 'emailId' : 'bhagat@freedom.com','team': 'KatyFlyers', 'mobileNumber': '+91-9493645007', 'voipNumber': '+71-402-544-4848', 'deskLocation': 'B6-064'};
 };
 
@@ -34,13 +36,29 @@ $scope.viewAccountSettingsFun = function(){
   $scope.viewAccountSettings = true;
 };
 
-$scope.search = function(){
+$scope.onClickSearchMenu = function(){
   resetFlags();
   $scope.searchAnything = true;
 }
 
-$scope.selectedCabin = function(){
-  $('#'+$scope.userDetails.deskLocation.replace('-','')).addClass('selectedPlace');
+$scope.search = function(){
+  //if(angular.isDefined($scope.searchType) && angular.isDefined($scope.searchPhrase)){
+    $scope.userResult = [{'name' : 'Firoz Khan', 'designation' : 'Senior Software Engineer', 'teams': ['Core', 'Iris'], 'deskLocation':'B6-063'},{'name' : 'Raveendra Yadav', 'designation' : 'Software Engineer', 'teams':['KatyFlyers'], 'deskLocation':'B6-065'}];
+  //} else alert('Please Select SearchCriteria');
 };
+
+$scope.getTeamDetails = function(team){
+  $scope.userResult = [{'name' : 'Firoz Khan', 'designation' : 'Senior Software Engineer', 'teams': ['Core', 'Iris'], 'deskLocation':'B6-063'},{'name' : 'Raveendra Yadav', 'designation' : 'Software Engineer', 'teams':['KatyFlyers'], 'deskLocation':'B6-065'},{'name' : 'Firoz Khan', 'designation' : 'Senior Software Engineer', 'teams': ['Core', 'Iris'], 'deskLocation':'B6-063'},{'name' : 'Raveendra Yadav', 'designation' : 'Software Engineer', 'teams':['KatyFlyers'], 'deskLocation':'B6-065'}];
+};
+
+$scope.selectedCabin = function(userdeskLocation){
+  $('span.link').removeClass('selectedPlace');
+  //have to add code to remove the class from previously selected place
+  $('#'+userdeskLocation.replace('-','')).addClass('selectedPlace');
+};
+
+/*$scope.onCloseDeskLocationModal = function(){
+  $('span.link').removeClass('selectedPlace');
+}*/
 init();
 }]);
